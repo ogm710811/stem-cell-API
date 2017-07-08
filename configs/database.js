87f12d1ebf00bf269f05887007629f74983aca05
ogm710811 <ogm710811@gmail.com>
2017-07-08 12:31:32 -0400

    database file update

--- configs/database.js
+++ configs/database.js
@@ -15,7 +15,7 @@ const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {  
-  console.log(`Connected to the ${process.env.MONGODB_URI} database`);
+  console.log(`Connected to the ${ process.env.MONGODB_URI } database`);
 });
 
 
